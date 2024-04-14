5a585eec48436549226a4516671d4750f6f18edb
PeterMotorniy <motorniy.peter@gmail.com>
2024-04-14 11:58:43 +0500

    Add lunge

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -1,59 +1,82 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MainCharacter : MonoBehaviour
 {
     // Настройки горизонтального движения
-    public Vector2 moveDirection;
-    public bool isFacedRight = true;
-    public float moveSpeed = 2f;
+    [SerializeField] private Vector2 moveDirection;
+    [SerializeField] private bool isFacedRight = true;
+    [SerializeField] private float moveSpeed = 0.1f;
     
     // Настройки вертикального движения
-    public float jumpForce = 10f;
-    public bool onGround;
-    public Transform groundCheck;
-    public LayerMask ground;
-    public float checkRadius = 0.2f;
-    public int currentJumps = 0;
-    public int maximumJumps = 2;
-    public DateTime lastJumped = DateTime.Now;
-    public TimeSpan jumpCooldown = TimeSpan.FromSeconds(1);
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private bool onGround;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float checkRadius = 0.2f;
+    [SerializeField] private int currentJumps = 0;
+    [SerializeField] private int maximumJumps = 2;
+    [SerializeField] private TimeSpan jumpCooldown = TimeSpan.FromSeconds(1);
+    [SerializeField] private DateTime LastJumped = DateTime.Now;
     
     // Настройки атаки
-    public int Punch1Force = 10;
-    public int Punch2Force = 20;
-    public TimeSpan Punch1Cooldown = TimeSpan.FromSeconds(1);
-    public TimeSpan Punch2Cooldown = TimeSpan.FromSeconds(2);
-    public Transform Punch1;
-    public float Punch1Radius;
-    public Transform Punch2;
-    public float Punch2Radius;
+    [SerializeField] private int punch1Force = 10;
+    [SerializeField] private int punch2Force = 20;
+    [SerializeField] private TimeSpan punch1Cooldown = TimeSpan.FromSeconds(1);
+    [SerializeField] private TimeSpan punch2Cooldown = TimeSpan.FromSeconds(2);
+    [SerializeField] private Transform punch1;
+    [SerializeField] private float punch1Radius;
+    [SerializeField] private Transform punch2;
+    [SerializeField] private float punch2Radius;
     
-    private Rigidbody2D rb;
-    private DateTime punch1LastUsed = DateTime.Now;
-    private DateTime punch2LastUsed = DateTime.Now;
+    //Настройки рывка
+    [SerializeField] private float lungeForce = 5f;
+    [SerializeField] private float lungeTimeInSeconds = 0.5f;
+    [SerializeField] private bool canLunge = true;
+    [SerializeField] private bool isLunging = false;
+    [SerializeField] private float lungeCooldownInSeconds = 2;
+    
+    private Rigidbody2D _rb;
+    private DateTime _punch1LastUsed = DateTime.Now;
+    private DateTime _punch2LastUsed = DateTime.Now;
 
     public void Start()    
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
     public void Update()
     {
+        if (isLunging)
+        {
+            return;
+        }
     }
 
     public void FixedUpdate()
     {
+        if (isLunging)
+        {
+            return;
+        }
+        
         Move();
         Jump();
         CheckingGround();
         Fight();
         Flip();
+        
+        if (Input.GetKeyDown(KeyCode.F) && canLunge)
+        {
+            StartCoroutine(Lunge());
+        }
     }
 
     private void Move()
     {
         moveDirection.x = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);    
+        _rb.position += new Vector2(moveDirection.x * moveSpeed, 0);    
     }
 
     private void Jump()
@@ -63,10 +86,10 @@ public class MainCharacter : MonoBehaviour
             currentJumps = 0;    
         }
         
-        if (Input.GetButton("Jump") && (onGround || (currentJumps < maximumJumps && lastJumped + jumpCooldown <= DateTime.Now)))
+        if (Input.GetButton("Jump") && (onGround || (currentJumps < maximumJumps && LastJumped + jumpCooldown <= DateTime.Now)))
         {
-            rb.velocity = Vector2.up * jumpForce;
-            lastJumped = DateTime.Now;
+            _rb.velocity = Vector2.up * jumpForce;
+            LastJumped = DateTime.Now;
             currentJumps++;
         }
         
@@ -79,16 +102,16 @@ public class MainCharacter : MonoBehaviour
 
     private void Fight()
     {
-        if(Input.GetButton("Fire1") && punch1LastUsed + Punch1Cooldown <= DateTime.Now)
+        if(Input.GetButton("Fire1") && _punch1LastUsed + punch1Cooldown <= DateTime.Now)
         {
-            punch1LastUsed = DateTime.Now;
-            FightHandler.Fight(Punch1.position, Punch1Radius, (int)LayersNumbers.Enemy, Punch1Force);
+            _punch1LastUsed = DateTime.Now;
+            FightHandler.Fight(punch1.position, punch1Radius, (int)LayersNumbers.Enemy, punch1Force);
         }
         
-        if(Input.GetButton("Fire2") && punch2LastUsed + Punch2Cooldown <= DateTime.Now)
+        if(Input.GetButton("Fire2") && _punch2LastUsed + punch2Cooldown <= DateTime.Now)
         {
-            punch2LastUsed = DateTime.Now;
-            FightHandler.Fight(Punch2.position, Punch2Radius, (int)LayersNumbers.Enemy, Punch2Force);
+            _punch2LastUsed = DateTime.Now;
+            FightHandler.Fight(punch2.position, punch2Radius, (int)LayersNumbers.Enemy, punch2Force);
         }
     }
 
@@ -101,5 +124,19 @@ public class MainCharacter : MonoBehaviour
             transform.localScale = newDirection;
             isFacedRight = !isFacedRight;
         }
+    }
+
+    private IEnumerator Lunge()
+    {
+        canLunge = false;
+        isLunging = true;
+        var gravity = _rb.gravityScale;
+        _rb.gravityScale = 0f;
+        _rb.velocity = new Vector2(transform.localScale.x * lungeForce, 0);
+        yield return new WaitForSeconds(lungeTimeInSeconds);
+        _rb.gravityScale = gravity;
+        isLunging = false;
+        yield return new WaitForSeconds(lungeCooldownInSeconds);
+        canLunge = true;
     }
 }
